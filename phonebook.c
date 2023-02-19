077b252c8145838e1c504fc5d1cf50be9fdb3f9e
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-19 21:36:02 +0000

    Sun, Feb 19, 2023, 9:36 PM +00:00

--- phonebook.c
+++ phonebook.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
+    // program has ask a user for a name, an age and a phone number//
+    // name - string //
+    // age - int //
+    // phone number - int //
+
 int main(void)
 {
-    //
-
+    // ask for a name (str)//
+    string name = get_string("What is your name?)
 }
