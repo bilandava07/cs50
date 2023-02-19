9951fe0cc6137af754586503c9cb8c11f049c8d4
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-19 23:31:52 +0000

    Mon, Feb 20, 2023, 12:31 AM +01:00

--- population.c
+++ population.c
@@ -6,7 +6,13 @@ int main(void)
     int m;
     int years = 0;
     // ask the user for a starting number of llamas
-    int n = get_int("How many llamas are there? ");
+    int n;
+    do
+    {
+        n = get_int("How many llamas are there? ");
+    }
+    while ( n < 9 );
+     n = get_int("How many llamas are there? ");
     // ask the user for a goal number of llamas
     int e = get_int("How many llamas should be there? ");
     do
