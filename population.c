abf1dc766f6148b65331587477d978db815da330
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-19 22:57:07 +0000

    Sun, Feb 19, 2023, 11:57 PM +01:00

---
+++ population.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // ask the user for a starting number of llamas
+    int n = get_int("How many llamas are there? ");
+    // ask the user for a goal number of llamas
+    int e = get_int("How many llamas should be there? ");
+    //add llamas every year
+    n = n + n/3
+    // substract llamas every year
+    n = n - n/4 
+
+
+}
