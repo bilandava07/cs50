8a29f2380dfa459de568ca9fc78dc97cb9d664b5
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-19 23:23:32 +0000

    Mon, Feb 20, 2023, 12:23 AM +01:00

--- population.c
+++ population.c
@@ -8,10 +8,11 @@ int main(void)
     int n = get_int("How many llamas are there? ");
     // ask the user for a goal number of llamas
     int e = get_int("How many llamas should be there? ");
-    while ( m < e )
+    do
     {
         m = n/3 + n;
         m = m - n/4;
     }
-    printf("The population is going to reach the desired number of llamas in %i years \n", m );
+    while ( m < e );
+    printf("The population is going to reach the desired number of llamas in %i years \n",  );
 }
