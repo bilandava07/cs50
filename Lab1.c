e3efcea93258a4385dc2b550e5b1a8d692041bb0
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-21 22:22:15 +0000

    Tue, Feb 21, 2023, 11:22 PM +01:00

---
+++ Lab1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //starting number
+    int n;
+    do
+    {
+        n = get_int("Starting number? ");
+    }
+    while ( n > 9 )
+}
