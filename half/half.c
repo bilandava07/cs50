b7da72f38ba51b0546a71892603dcc5aa325ab2c
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-21 23:09:25 +0000

    Wed, Feb 22, 2023, 12:09 AM +01:00

--- half/half.c
+++ half/half.c
@@ -3,6 +3,25 @@
 
 int main(void)
 {
-    //bill before input float
-    float bill = get_float("Bill before taxes and tip? ");
+    //ask for bill before input float
+    do
+    {
+        float bill = get_float("Bill before taxes and tip: ");
+    }
+    while ( bill < 0 );
+
+    // ask for sale tax percent
+    do
+    {
+        float tax = get_float("Sale Tax Percent: ");
+    }
+    while ( tax < 0 );
+
+    // ask for tip percent
+    do
+    {
+        int tip = get_int("Tip percent: ");
+    }
+    while ( tip < 0 );
+
 }
