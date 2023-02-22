d6947b484dc03b55b4434cf5aac34ff5d128107e
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-22 00:48:50 +0000

    Wed, Feb 22, 2023, 1:48 AM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // ask for the height input int ( h = height )form 1 to 8 inclusively
+    do
+    {
+        int h = get_int("Height: ");
+    }
+    while
+
+
 }
