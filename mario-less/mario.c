281b6898e8aa9107d0d4f9b6eb0485a340c801be
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-22 01:08:47 +0000

    Wed, Feb 22, 2023, 2:08 AM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,15 +5,15 @@ int main(void)
 {
     // ask for the height input int ( h = height )from 1 to 8 inclusively
     int h;
-    char #;
     do
     {
         h = get_int("Height: ");
     }
     while ( h < 1 || h > 8 );
     // print a vertical row H
-    for (int i = 0; i < h; i++)
+    switch(h)
     {
-        printf(\n);
+        case h:
+        printf("#");
     }
 }
