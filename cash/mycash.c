b263ca6e53b407a653f75a3fe8d0209ef1270700
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-23 22:07:34 +0000

    Thu, Feb 23, 2023, 11:07 PM +01:00

--- cash/mycash.c
+++ cash/mycash.c
@@ -1,14 +1,41 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int coins = 0;
+int cents;
+
 int main(void)
 {
-    int cents;
     do
     {
         cents = get_int("Change owed: ");
     }
     while ( cents < 0);
-    
+// calculate quarters
+    while ( cents >= 25)
+    {
+        cents = cents - 25;
+        coins++;
+    }
+
+    while ( cents >= 10)
+    {
+        cents = cents - 10;
+        coins++;
+    }
+
+    while ( cents >= 5)
+    {
+        cents = cents - 5;
+        coins++;
+    }
+
+    while ( cents >= 1)
+    {
+        cents = cents - 1;
+        coins++;
+    }
+    printf("%i \n", coins);
+
 
 }
