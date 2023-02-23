72685348e62abe15e852921121bbb4db9626236d
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-23 20:06:10 +0000

    Thu, Feb 23, 2023, 9:06 PM +01:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,9 +10,6 @@ int main(void)
         h = get_int("Height: ");
     }
     while ( h < 1 || h > 8 );
-    // 
-    switch (h)
-    {
-        case
-    }
+    // printing out (loops)
+    for ( )
 }
