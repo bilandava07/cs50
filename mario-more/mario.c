db27ff6dafa2c7df73ad72ca13a974a7daeccea0
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-23 21:14:55 +0000

    Thu, Feb 23, 2023, 10:14 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int h;
+    do
+    {
+        h = get_int("Height: \n");
+    }
+    while ( h < 1 || h > 8);
+
+
 }
