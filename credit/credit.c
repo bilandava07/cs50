5d8c2c779e6a96d53cbe146ffc2e31a3939cf259
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-02-24 12:36:54 +0000

    Fri, Feb 24, 2023, 1:36 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,19 +6,27 @@ int main(void)
     //promt for input
         long card;
         card = get_long("Number: ");
-    if ( card < 1000000000000 || card > 9999999999999999)
+    if ( card < 1000000000000 || card > 9999999999999999) // number is not a creditcard
     {
         printf("INVALID \n");
     }
-    else
+    else // calculate the checksum
     {
-        //calculate the checksum
         int mod = 0;
-        for ( int i = 0; i > 16; i++)
-        {
-            card = 
+        int d1;
+        int d2;
+        int d3;
+        int d4;
+        int d5;
+        int d6;
+        int d7;
+        int d8;
 
-        }
+        d1 = card % mod;
+        mod= mod *2;
+        d2 = card % mod;
+
+        printf("%i %i \n" d1, d2);
+
     }
-
 }
