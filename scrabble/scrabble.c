a8a73106850c5d9e550166c8f831b80ff5cf8301
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-02 13:42:53 +0000

    Sun, Apr 2, 2023, 3:42 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -12,11 +12,11 @@ int main(void)
 {
     // Get input words from both players
     string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    //string word2 = get_string("Player 2: ");
 
     // Score both words
     int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+    //int score2 = compute_score(word2);
 
     // TODO: Print the winner
 }
@@ -27,10 +27,12 @@ int compute_score(string word)
 
     for (int i = 0; i < length; i++)
     {
-        printf("%c", word[i]);
+        printf("%i ", word[i]);
+
+        int letters 
 
     }
-    printf("%c \n", word[0]);
+    printf("\n");
     return 0;
 
 
