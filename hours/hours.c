293085fd4197bff996b41d30469480499f3fb870
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-04 13:11:49 +0000

    Tue, Apr 4, 2023, 3:11 PM +02:00

--- hours/hours.c
+++ hours/hours.c
@@ -24,8 +24,8 @@ int main(void)
     printf("%.1f hours\n", calc_hours(hours, weeks, output));
 }
 
-// TODO: complete the calc_hours function
+
 float calc_hours(int hours[], int weeks, char output)
 {
-
+    return 0;
 }
