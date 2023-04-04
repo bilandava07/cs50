2e2d3ddc72e84e411d60423cf3d6cb0d5b57814c
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-04 11:47:31 +0000

    Tue, Apr 4, 2023, 1:47 PM +02:00

--- no_vowels/no-vowels.c
+++ no_vowels/no-vowels.c
@@ -5,10 +5,38 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+string replace(string word);
+int length;
 
 int main(int argc, string argv[])
 {
-    printf("%i \n", argc);
-    printf("%s \n", argv[1]);
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word \n");
+        return 1;
+        length = strlen(argv[1]);
+        int word = replace(argv[1])
 
+    }
+}
+
+string replace(string word)
+{
+    for (int i = 0; i < length; i++)
+    {
+        switch(word[i])
+        {
+            case e:
+                word[i] = 3;
+            case a:
+                word[i] = 6;
+            case i:
+                word[i] = 1;
+            case o:
+                word[i] = 0;
+        }
+    }
+    return word;
 }
