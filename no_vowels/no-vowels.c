1dd7aac83532ac1b84e1e354afe321aeaf62a300
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-04 11:35:10 +0000

    Tue, Apr 4, 2023, 1:35 PM +02:00

---
+++ no_vowels/no-vowels.c
@@ -0,0 +1,14 @@
+// Write a function to replace vowels with numbers
+// Get practice with strings
+// Get practice with command line
+// Get practice with switch
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    printf("%i \n", argc);
+    printf("%s \n", argv[1]);
+
+}
