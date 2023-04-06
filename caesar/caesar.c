7b05b9829c079b95e834e0c6b2a3b772acb9aa32
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-06 21:28:54 +0000

    Thu, Apr 6, 2023, 11:28 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool only_digits (string key_input)
+bool only_digits (string key);
 
 int main(int argc, string argv[])
 {
@@ -10,10 +10,15 @@ int main(int argc, string argv[])
         printf("Usage: ./ caesar key \n");
         return 1;
     }
-    
+    bool check = only_digits(argv[1]);
+    if ( check == false)
+    {
+        printf("Usage: ./ caesar key \n");
+        return 1;
+    }
 }
 
-bool only_digits (string key_input)
+bool only_digits (string key)
 {
-    if ()
+    if ( key )
 }
