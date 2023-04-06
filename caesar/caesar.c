54f83ea4a05cc79a02e163e3f2448a1cf67dc2bf
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-06 21:18:22 +0000

    Thu, Apr 6, 2023, 11:18 PM +02:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
+    do
+    {
+        return 1;
+        printf("Usage: ./ caesar key \n");
+    }
+    while ( argc != 1);
 
 }
