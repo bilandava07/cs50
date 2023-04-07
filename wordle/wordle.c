cd4150d889d9009074368f39701eff1711db2a47
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-07 16:16:57 +0000

    Fri, Apr 7, 2023, 6:16 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -37,13 +37,12 @@ int main(int argc, string argv[])
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    
+    wordsize = atoi(argv)
     if (argv[1] != "5", != "6", != "7", != "8")
     {
         printf("Error: wordsize must be either 5, 6, 7, or 8 \n");
         return 1;
     }
-    wordsize = argv[1];
 
     // open correct file, each file has exactly LISTSIZE words
     char wl_filename[6];
