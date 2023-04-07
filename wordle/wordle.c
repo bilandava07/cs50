718c4c1fa52ceb3511b9f5c7cd0e9c52a51f35ee
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-07 20:24:55 +0000

    Fri, Apr 7, 2023, 10:24 PM +02:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -170,7 +170,13 @@ void print_word(string guess, int wordsize, int status[])
 {
     // print word character-for-character with correct color coding, then reset terminal font to normal
     // TODO #6
-    
+    for (int i = 0; i < strlen(guess); i++)
+    {
+        if (status[i] == 2)
+        {
+            printf(GREEN")
+        }
+    }
 
     printf("\n");
     return;
