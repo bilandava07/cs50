786c6a9c14c3d28c3c254c484f0171f4e3868138
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-20 19:22:34 +0000

    Thu, Apr 20, 2023, 9:22 PM +02:00

--- mltprecursion/mr.c
+++ mltprecursion/mr.c
@@ -1,19 +1,30 @@
-#include <std.h>
+#include <stdio.h>
 #include <cs50.h>
 
 int collatz(int n);
+int steps = 0;
 
 int main(void)
 {
-    int n = get_int("n: \n");
-    printf(" It takes %i steps \n", collatz(n));
+    int n = get_int("n: ");
+    printf("It takes %i steps \n", collatz(n));
 
 }
 
 int collatz(int n)
 {
-    if n == 1
+    if (n == 1)
         return 0;
-    else if n 
 
+    else if(n % 2 == 0)
+    {
+        collatz(n/2);
+        steps++;
+    }
+    else
+    {
+        collatz(n*3 + 1);
+        steps++;
+    }
+    return steps;
 }
