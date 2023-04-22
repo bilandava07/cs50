528f76e97c63bc7b25b5eac3457f3710c05cdd02
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-22 12:03:09 +0000

    Sat, Apr 22, 2023, 2:03 PM +02:00

---
+++ section/factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int factorial(int number);
+
+int main(void)
+{
+    int n = get_int("Type a number: ");
+    printf("%i\n", factorial(n));
+}
+
+int factorial(int number)
+{
+    
+
+}
