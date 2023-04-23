79e7bfb3b40e8a1c7703c7fc769e52b7c9c4ffeb
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-23 13:18:36 +0000

    Sun, Apr 23, 2023, 3:18 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -84,7 +84,7 @@ void print_winner(void)
     {
         if (candidates[i].votes > candidates[i+1].votes)
         {
-            candidates[i].votes = candidates[i+1].votes
+            candidates [i].votes = candidates[i+1].votes
 
         }
     }
