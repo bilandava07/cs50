c5b135e4e982c2680bd94f4f97ac1e56315c6591
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-23 09:45:10 +0000

    Sun, Apr 23, 2023, 11:45 AM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,13 +66,10 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    for (int i = 0; i < candidate_count; i++);
+    if (candidates[i].name == name)
     {
-        if (candidates[i].name == name)
-        {
-            candidates[i].votes++;
-            return true;
-        }
+        candidates[i].votes++;
+        return true;
     }
     return false;
 }
@@ -80,6 +77,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    
+
     return;
 }
