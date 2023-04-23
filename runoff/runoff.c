0641d29ae12aba6c896416fc59a07995e075eccd
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-23 21:10:41 +0000

    Sun, Apr 23, 2023, 11:10 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -164,10 +164,9 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int majority = voter_count / 2;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes >= majority)
+        if (candidates[i].votes > voter_count / 2)
         {
             printf("The winner is: %s\n",candidates[i].name);
             return true;
