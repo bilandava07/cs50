862bd46d13d7de635190d633dc13863be07c70cf
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-23 13:28:13 +0000

    Sun, Apr 23, 2023, 3:28 PM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,7 +80,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-        printf("%i %i %i\n", candidates[i].votes, candidates[i+1].votes, candidates[i+2].votes);
+        printf("%i %i %i\n", candidates[0].votes, candidates[1].votes, candidates[2].votes);
     int winner;
     for (int i = 0; i < candidate_count; i++)
     {
