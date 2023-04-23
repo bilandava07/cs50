c4a0b33616c942e3f7540ff39d1fc3951dfe301d
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-23 18:42:29 +0000

    Sun, Apr 23, 2023, 8:42 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -128,7 +129,14 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    for (int i = 0; i < )
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i].name) !== 0)
+        {
+            return false;
+        }
+    }
+    
     return false;
 }
 
