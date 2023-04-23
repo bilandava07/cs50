679c6708097f0a3f7d988516013463e8b6cbbd99
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-23 21:29:10 +0000

    Sun, Apr 23, 2023, 11:29 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -196,15 +196,12 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
+    int tie_cand[];
+    bool tie = false;
     for (int i = 0; i < candidate_count; i++)
     {
-        bool tie = false;
-        if (candidates[i].eliminated == false && candidates[i + 1].eliminated == false)
+        if (candidates[i].eliminated == false)
         {
-            if (candidates[i].votes == candidates[i + 1].votes)
-            {
-                
-            }
 
         }
 
