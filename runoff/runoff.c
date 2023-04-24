c640af741d6865a134f674ecb74a879b04909b1b
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-24 17:00:19 +0000

    Mon, Apr 24, 2023, 7:00 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -181,6 +181,10 @@ int find_min(void)
     int min;
     for (int i = 0; i < candidate_count; i++)
     {
+        if
+        {
+            
+        }
         if (candidates[i].eliminated == false)
         {
             if (candidates[i + 1].votes < min)
