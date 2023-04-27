d6020c3144301f872319f8382b16541df1e04976
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-27 18:58:57 +0000

    Thu, Apr 27, 2023, 8:58 PM +02:00

--- temps/temps.c
+++ temps/temps.c
@@ -66,7 +66,7 @@ void sort_cities(void)
     int min = temps[0];
     for (int i = 1; i < n - 1; i++)
     {
-        for (int j = 1; j < n - 1; j++)
+        for (int j = 1; j < n; j++)
         {
             if (temps[j] < min)
             {
