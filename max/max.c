0c76f838bd45c00865462f84b07b2563f24d97f6
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-04-27 18:41:50 +0000

    Thu, Apr 27, 2023, 8:41 PM +02:00

--- max/max.c
+++ max/max.c
@@ -28,9 +28,9 @@ int main(void)
 int max(int array[], int n)
 {
     int max = array[0];
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        if (array [i + 1] > max)
+        if (array [i] > max)
             max = array [i + 1];
     }
     return max;
