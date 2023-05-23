ddc17312fc6084af7296e85899246e122e30b77b
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-05-23 19:39:45 +0000

    Tue, May 23, 2023, 9:39 PM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,58 +2,16 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE pixel1 = image[2][0];
-    pixel1.rgbtBlue = 255;
-
-    image[2][0].rgbtBlue = 255;
-    image[3][0].rgbtBlue = 255;
-    image[4][0].rgbtBlue = 255;
-    image[5][0].rgbtBlue = 255;
-
-    image[2][7].rgbtBlue = 255;
-    image[3][7].rgbtBlue = 255;
-    image[4][7].rgbtBlue = 255;
-    image[5][7].rgbtBlue = 255;
-
-    image[7][2].rgbtBlue = 255;
-    image[7][3].rgbtBlue = 255;
-    image[7][4].rgbtBlue = 255;
-    image[7][5].rgbtBlue = 255;
-
-    image[0][2].rgbtBlue = 255;
-    image[0][3].rgbtBlue = 255;
-    image[0][4].rgbtBlue = 255;
-    image[0][5].rgbtBlue = 255;
-
-    image[2][0].rgbtBlue = 255;
-    image[3][0].rgbtBlue = 255;
-    image[4][0].rgbtBlue = 255;
-    image[5][0].rgbtBlue = 255;
-
-    image[1][1].rgbtBlue = 255;
-
-    image[1][6].rgbtBlue = 255;
-
-    image[6][1].rgbtBlue = 255;
-
-    image[6][6].rgbtBlue = 255;
-
-    image[2][2].rgbtBlue = 255;
-
-    image[2][5].rgbtBlue = 255;
-
-    image[4][2].rgbtBlue = 255;
-
-    image[4][5].rgbtBlue = 255;
-
-    image[5][3].rgbtBlue = 255;
-
-    image[5][4].rgbtBlue = 255;
-
-
-
-
-
-
-
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
+            {
+                image[i][j].rgbtRed = 0xff;
+                image[i][j].rgbtGreen = 0x00;
+                image[i][j].rgbtBlue = 0x00;
+            }
+        }
+    }
 }
