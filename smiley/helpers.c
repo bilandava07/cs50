9c95d07f1e9ebd7c308dbc653b7470f1388f2e71
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-05-23 19:24:17 +0000

    Tue, May 23, 2023, 9:24 PM +02:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -5,9 +5,6 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     RGBTRIPLE pixel1 = image[2][0];
     pixel1.rgbtBlue = 255;
 
-    RGBTRIPLE pixel1 = image[3][0];
-    pixel1.rgbtBlue = 255;
-
-
-
+    image[3][0].rgbtBlue = 255;
+    image[4][0].rgbtBlue = 255;
 }
