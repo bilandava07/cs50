81035d20d5e3bfabe15a8c43f9a46c185abf7c70
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-06-08 17:41:57 +0000

    Thu, Jun 8, 2023, 7:41 PM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,9 +41,7 @@ int main(int argc, char *argv[])
 
 
     int16_t buffer;
-
-
-    while    = (fread(&buffer, sizeof(int16_t),1, input)) != EOF);
+    fread(&buffer, sizeof(int16_t),1, input)
 
 
 
