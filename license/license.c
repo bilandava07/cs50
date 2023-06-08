5f6c7ff5e3ff6c27c99a5399c1d513f546735195
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-06-08 18:37:35 +0000

    Thu, Jun 8, 2023, 8:37 PM +02:00

--- license/license.c
+++ license/license.c
@@ -25,7 +25,11 @@ int main(int argc, char *argv[])
         buffer[6] = '\0';
 
         // Save plate number in array
-        plates[idx] = buffer;
+        for(int i = 0; i < 7; i++)
+        {
+            plates[idx].[i] = buffer[i];
+
+        }
         idx++;
     }
 
