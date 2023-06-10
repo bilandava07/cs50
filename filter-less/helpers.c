8c658a7425afd7c38fa3f09a35c33c021301aeaa
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-06-10 21:03:01 +0000

    Sat, Jun 10, 2023, 11:03 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -18,6 +18,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtRed = image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue *
+        }
+    }
     return;
 }
 
