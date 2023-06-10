0529239d26319977bfb8eb6e2e2daca59930dd41
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-06-10 21:09:04 +0000

    Sat, Jun 10, 2023, 11:09 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,7 +22,23 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue *
+            image[i][j].rgbtRed = (int) round(image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue * 0.189);
+            if (image[i][j].rgbtRed > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+
+            image[i][j].rgbtGreen = (int) round(image[i][j].rgbtRed * 0.349 + image[i][j].rgbtGreen * 0.686 + image[i][j].rgbtBlue * 0.168);
+            if (image[i][j].rgbtGreen > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+
+            image[i][j].rgbtBlue = (int) round(image[i][j].rgbtRed * 0.272 + image[i][j].rgbtGreen * 0.534 + image[i][j].rgbtBlue * 0.131);
+            if (image[i][j].rgbtBlue > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
         }
     }
     return;
