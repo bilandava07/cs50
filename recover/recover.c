a09d72271607e6567c0e460e4a0064cddf0842a1
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-06-16 22:07:39 +0000

    Sat, Jun 17, 2023, 12:07 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 1)
+    {
+        printf("Correct usage: ./recover <name of the file>");
+        return 1;
+    }
+    
+
+
 }
