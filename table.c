560a6ff896adfb2b02cc511c3e8f412a7fd3f4b6
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-11-20 01:41:13 +0000

    Mon, Nov 20, 2023, 2:41 AM +01:00

---
+++ table.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+}
+node;
+
+node *table[26];
+
+int hash(string phrase);
+
+int main(void)
+{
+
+}
+
+
+int hash(string phrase)
+{
+    
+}
