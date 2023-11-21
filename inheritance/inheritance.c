35650009b7547437c36ab7eb852f80fd6ab5f0e9
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2023-11-21 02:27:02 +0000

    Tue, Nov 21, 2023, 3:27 AM +01:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -85,8 +85,8 @@ void free_family(person *p)
     // TODO: Free parents recursively
     else
     {
-        free_family(p->parents[0])
-        
+        free_family(p->parents[0]);
+
     }
     // TODO: Free child
 
