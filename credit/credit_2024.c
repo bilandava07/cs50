da361a0e25ad48de100d1a7b534ad43169783d9f
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-18 21:48:30 +0000

    Sun, Aug 18, 2024, 11:48 PM +02:00

--- credit/credit_2024.c
+++ credit/credit_2024.c
@@ -3,11 +3,29 @@
 
 int main (void)
 {
-    int counter = 0;
+    int counter;
     long number = get_long number("Number: ");
 
+    int right_most;
+    int other;
     while (number != 0)
     {
+        right_most += (number % 10);
+        number = number / 10;
+
+        tmp_other = number % 10;
+        tmp_other *= 2;
+
+        if (tmp_other > 9)          // (10-18)
+        {
+            other += (tmp_other % 10);
+            tmp_other = tmp_other / 10;
+            other += tmp_other;
+        }
+        else                        // (0-9)
+        {
+            other += tmp_other;
+        }
         
     }
 
