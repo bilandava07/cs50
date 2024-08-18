d358b771bf27c30d49a655c52b265e3aef3fa339
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-18 22:05:02 +0000

    Mon, Aug 19, 2024, 12:05 AM +02:00

--- credit/credit_2024.c
+++ credit/credit_2024.c
@@ -12,25 +12,30 @@ int main (void)
     {
         right_most += (number % 10);
         number = number / 10;
+        counter++;
 
-        int tmp_other = number % 10;
-        number = number / 10;
-        tmp_other *= 2;
+        if (number !=0)
+        {
+            int tmp_other = number % 10;
+            number = number / 10;
+            tmp_other *= 2;
 
-        if (tmp_other > 9)          // (10-18)
-        {
-            other += (tmp_other % 10);
-            tmp_other = tmp_other / 10;
-            other += tmp_other;
-        }
-        else                        // (0-9)
-        {
-            other += tmp_other;
+            if (tmp_other > 9)          // (10-18)
+            {
+                other += (tmp_other % 10);
+                tmp_other = tmp_other / 10;
+                other += tmp_other;
+            }
+            else                        // (0-9)
+            {
+                other += tmp_other;
+            }
         }
 
     }
     int sum = right_most + other;
 
-    printf("%i", sum);
+    printf("sum = %i\n", sum);
+    printf("The number is %i digits long.\n", counter);
 
 }
