ec2802599f717237c22817ef79f2ef40f470aa1a
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-20 20:00:52 +0000

    Tue, Aug 20, 2024, 10:00 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,11 +3,23 @@
 #include <string.h>
 
 
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
 
 int main (void)
 {
     string text = get_string("Text: ");
 
-    
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 
+
+}
+
+int count_letters(text)
+{
+    
 }
