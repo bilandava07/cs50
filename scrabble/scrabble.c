394ec2f9091db8b0a42f25a1ec0a41a89ce5a2e3
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-20 19:37:54 +0000

    Tue, Aug 20, 2024, 9:37 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -4,15 +4,15 @@
 #include <string.h>
 
 int calculate_score(string word);
-int points = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+int points [] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
 int main(void)
 {
     string p1 = get_string("Player1: ");
     string p2 = get_string("Player2: ");
 
-    p1_score = calculate_score(p1)
-    p2_score = calculate_score(p2)
+    int p1_score = calculate_score(p1);
+    int p2_score = calculate_score(p2);
 
     if (p1_score > p2_score)
     {
