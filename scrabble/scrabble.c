88c01120b6c2c51940c82793d871129845e8d1f1
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-20 19:22:21 +0000

    Tue, Aug 20, 2024, 9:22 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,12 +2,23 @@
 #include <cs50.h>
 #include <ctype.h>
 
+int calculate_score(string word);
+int points = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
 int main(void)
 {
     string p1 = get_string("Player1: ");
     string p2 = get_string("Player2: ");
 
+    p1_score = calculate_score(p1)
+    p2_score = calculate_score(p2)
 
-    
+    if (p1_score > p2_score)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (p1_score < p2_score)
+    {
+        printf("Player 2 wins!\n");
+    }
 }
