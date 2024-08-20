9c7ab4f38b6e10d0c64452c5ed69b43948c332b5
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-20 19:36:35 +0000

    Tue, Aug 20, 2024, 9:36 PM +02:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <string.h>
 
 int calculate_score(string word);
 int points = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
@@ -29,5 +30,10 @@ int main(void)
 
 int calculate_score(string word)
 {
-    
+    int sum = 0;
+    for (int i = 0, length = strlen(word); i < length; i++)
+    {
+        sum += word[i] - 65 + points[i];
+    }
+    return sum;
 }
