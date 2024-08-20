17e40850c48d9a6a516a73837e7a995d581cbdfc
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-20 20:14:30 +0000

    Tue, Aug 20, 2024, 10:14 PM +02:00

--- readability/readability.c
+++ readability/readability.c
@@ -3,27 +3,15 @@
 #include <string.h>
 
 
-int count_letters(string text);
-int count_words(string text);
-int count_sentences(string text);
-
-
 int main (void)
 {
     string text = get_string("Text: ");
 
-    int letters = count_letters(text);
-    int words = count_words(text);
-    int sentences = count_sentences(text);
-
-
-}
-
-int count_letters(text)
-{
     int letters = 0;
     int words = 0;
     int sentences = 0;
+
+    //count everything in one loop instead of going through the same string 3 times with different functions (my inital approach)
     for (int i = 0, length = strlen(text); i < length; i++)
     {
         if (isalpha(text[i]))
@@ -36,7 +24,15 @@ int count_letters(text)
         }
         else if (text[i] == '!' or text[i] == '.' or text[i] == '?')
         {
-            sentences
+            sentences++;
         }
     }
+
+    //add 1 to words, as there is no blank space at the end of the string
+    words++;
+
+    int average
+
 }
+
+
