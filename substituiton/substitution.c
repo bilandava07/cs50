376ff66b96c43832d6cf36657cc539599f75aa33
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-08-20 21:50:31 +0000

    Tue, Aug 20, 2024, 11:50 PM +02:00

--- substituiton/substitution.c
+++ substituiton/substitution.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else if (strlen(argv[1]) != 26)
+    {
+        printf("Key must contain 26 charachters\n");
+        return 1;
+    }
+
+    for (int i = 0, length = strlen(argv[1]); i < length; i++)
+    {
+        if (isalpah(argv[1][i])
+    }
+}
