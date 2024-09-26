30b29c6982e9894626da07e8bdfd34ef8d07249c
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-09-26 13:06:11 +0000

    Thu, Sep 26, 2024, 3:06 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -114,7 +114,7 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count-1; i++)
     {
-        for (int z = 0; z < candidate_count; z++)
+        for (int z = 0; z < candidate_count-1; z++)
         {
             preferences[i][z+1]++;
         }
