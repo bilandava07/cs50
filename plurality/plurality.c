5f37744e1137beb1a10b7569e9d764d4a816dd54
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-09-26 09:25:42 +0000

    Thu, Sep 26, 2024, 11:25 AM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -88,5 +88,12 @@ void print_winner(void)
             max_votes = candidates[i].votes;
         }
     }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
     return;
 }
