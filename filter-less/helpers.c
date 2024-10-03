5392484a93d15814d1a27efe67ecdcbadd5cff1c
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-03 23:00:04 +0000

    Fri, Oct 4, 2024, 1:00 AM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,7 +3,7 @@
 
 
 int lesser (int a, int b);
-void avg_set (int start, int end, int block_height, int block_width, int *arr, int *copy);
+void avg_set (int i, int y, int start, int end, int block_height, int block_width, int *image, int *copy);
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -171,8 +171,15 @@ int lesser (int a, int b)
         return b;
 }
 
-void avg_set (int start, int end, int block_height, int block_width, int *image, int *copy)
+void avg_set (int i, int y, int start, int end, int block_height, int block_width, int *image, int *copy)
 {
+    int sum_blue = 0;
+    int sum_green = 0;
+    int sum_red = 0;
+
+    start = i - 1;
+    end = y - 1;
+
     for (int z = start; z < start + block_height; z++)
     {
         {
