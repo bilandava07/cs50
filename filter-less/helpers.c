1ca6abf72ba427a757f1a9d826c9a1368c234443
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-03 11:35:17 +0000

    Thu, Oct 3, 2024, 1:35 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,12 +3,16 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int avg;
     for (int i = 0; i < height; i++)
     {
         for (int y = 0; y < width; y++)
         {
-            avg = i
-            image[i][y].rgbtBlue =
+            avg = (image[i][y].rgbtBlue + image[i][y].rgbtGreen + image[i][y].rgbtRed) / 3;
+
+            image[i][y].rgbtBlue = avg;
+            image[i][y].rgbtGreen = avg;
+            image[i][y].rgbtRed = avg;
         }
     }
     return;
@@ -17,6 +21,30 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int y = 0; y < width; y++)
+        {
+            int red = int(.393 * image[i][y].rgbtRed + .769 * image[i][y].rgbtGreen + .189 * image[i][y].rgbtBlue);
+            int green = int(.349 * image[i][y].rgbtRed + .686 * image[i][y].rgbtGreen + .168 * image[i][y].rgbtBlue);
+            int blue = int(.272 * image[i][y].rgbtRed + .534 * image[i][y].rgbtGreen + .131 * image[i][y].rgbtBlue);
+
+            if (red > 255)
+                image[i][y].rgbtRed = 255
+            else
+                image[i][y].rgbtRed = red;
+
+            if (green > 255)
+                image[i][y].rgbtGreen = 255;
+            else
+                image[i][y].rgbtGreen = green;
+
+            if (blue > 255)
+                image[i][y].rgbtBlue = 255;
+            else
+                image[i][y].rgbtBlue = blue;
+        }
+
     return;
 }
 
