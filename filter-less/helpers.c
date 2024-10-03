751edd1a1109d835c604548d00c619c6002aae3a
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-03 11:06:09 +0000

    Thu, Oct 3, 2024, 1:06 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,128 +1,25 @@
 #include "helpers.h"
-#include <math.h>
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
-        {
-                float avrg = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
-                int rounded_avrg = (int) round(avrg);
-                image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = rounded_avrg;
-        }
-    }
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
-        {
-            float sepiaRed = (image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue * 0.189);
-
-            float sepiaGreen  = (image[i][j].rgbtRed * 0.349 + image[i][j].rgbtGreen * 0.686 + image[i][j].rgbtBlue * 0.168);
-
-            float sepiaBlue = (image[i][j].rgbtRed * 0.272 + image[i][j].rgbtGreen * 0.534 + image[i][j].rgbtBlue * 0.131);
-
-            if ((int) round(sepiaRed) > 255)
-            {
-                image[i][j].rgbtRed = 255;
-            }
-            else
-            {
-                image[i][j].rgbtRed = (int) round(sepiaRed);
-            }
-
-
-            if ((int) round(sepiaGreen) > 255)
-            {
-                image[i][j].rgbtGreen = 255;
-            }
-            else
-            {
-                image[i][j].rgbtGreen = (int) round(sepiaGreen);
-            }
-
-            if ((int) round(sepiaBlue ) > 255)
-            {
-                image[i][j].rgbtBlue = 255;
-            }
-            else
-            {
-                image[i][j].rgbtBlue = (int) round(sepiaBlue);
-            }
-
-        }
-    }
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int iter = width / 2;
-    for(int i = 0; i < height; i++)
-    {
-        int left = 0;
-        int right = width - 1;
-        RGBTRIPLE ph;
-
-        for(int j = 0; j < iter; j++)
-        {
-            ph.rgbtRed = image[i][left].rgbtRed;
-            ph.rgbtGreen = image[i][left].rgbtGreen;
-            ph.rgbtBlue = image[i][left].rgbtBlue;
-
-            image[i][left].rgbtRed = image[i][right].rgbtRed;
-            image[i][left].rgbtGreen = image[i][right].rgbtGreen;
-            image[i][left].rgbtBlue = image[i][right].rgbtBlue;
-
-            image[i][right].rgbtRed = ph.rgbtRed;
-            image[i][right].rgbtGreen = ph.rgbtGreen;
-            image[i][right].rgbtBlue = ph.rgbtBlue;
-
-            left++;
-            right--;
-        }
-
-    }
-
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    //create the copy of the 2D array "image"
-    RGBTRIPLE copy[height][width];
-
-    for(int i = 0; i < height; i++)
-    {
-        for(int j = 0; j < width; j++)
-        {
-            copy[i][j].rgbtRed = image[i][j].rgbtRed;
-            copy[i][j].rgbtGreen = image[i][j].rgbtGreen;
-            copy[i][j].rgbtBlue = image[i][j].rgbtBlue;
-        }
-    }
-    // blur
-    for(int i = 1; i < height - 1; i++)
-    {
-        for(int j = 1; j < width - 1; j++)
-        {
-            float blurRed = (copy[i-1][j-1].rgbtRed + copy[i-1][j].rgbtRed + copy[i-1][j+1].rgbtRed + copy[i][j-1].rgbtRed + copy[i][j].rgbtRed + copy[i][j+1].rgbtRed + copy[i+1][j-1].rgbtRed + copy[i+1][j].rgbtRed + copy[i+1][j+1].rgbtRed) / 9.0;
-            float blurGreen = (copy[i-1][j-1].rgbtGreen + copy[i-1][j].rgbtGreen + copy[i-1][j+1].rgbtGreen + copy[i][j-1].rgbtGreen + copy[i][j].rgbtGreen + copy[i][j+1].rgbtGreen + copy[i+1][j-1].rgbtGreen + copy[i+1][j].rgbtGreen + copy[i+1][j+1].rgbtGreen) / 9.0;
-            float blurBlue = (copy[i-1][j-1].rgbtBlue + copy[i-1][j].rgbtBlue + copy[i-1][j+1].rgbtBlue + copy[i][j-1].rgbtBlue + copy[i][j].rgbtBlue + copy[i][j+1].rgbtBlue + copy[i+1][j-1].rgbtBlue + copy[i+1][j].rgbtBlue + copy[i+1][j+1].rgbtBlue) / 9.0;
-
-            image[i][j].rgbtRed = (int) round(blurRed);
-            image[i][j].rgbtGreen = (int) round(blurGreen);
-            image[i][j].rgbtBlue = (int) round(blurBlue);
-        }
-    }
-
     return;
 }
