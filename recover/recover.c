19efe614522dfd70bd552a3238aeada9395a9986
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-05 12:03:41 +0000

    Sat, Oct 5, 2024, 2:03 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCK 512
+
 int main(int argc, char *argv[])
 {
+    // Accept a single command-line argument
     if (argc != 2)
     {
         printf("Usage: ./recover [name of the forensic image]");
     }
+
+    // Open the memory card file for reading
+
+    
 }
