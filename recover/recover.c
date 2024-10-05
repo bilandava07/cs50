f46ea54c0dc45f7eb84e296abab68c871f37338a
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-05 12:08:16 +0000

    Sat, Oct 5, 2024, 2:08 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,10 +8,16 @@ int main(int argc, char *argv[])
     // Accept a single command-line argument
     if (argc != 2)
     {
-        printf("Usage: ./recover [name of the forensic image]");
+        printf("Usage: ./recover [name of the forensic image]\n");
     }
 
     // Open the memory card file for reading
 
+    FILE *card = fopen( "card.raw", "r");
+    if (card == NULL)
+    {
+        printf("Could not open the file!\n");
+        return 2;
+    }
     
 }
