df30ec5ceb531c6eef121071d359d060a90c3582
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-09 21:17:57 +0000

    Wed, Oct 9, 2024, 11:17 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -41,6 +41,11 @@ person *create_family(int generations)
     // TODO: Allocate memory for new person
     person *new_person = malloc(sizeof(person));
 
+    if (new_person == NULL)
+    {
+        printf("Error allocating memory for a new person\n");
+        return 2;
+    }
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -49,6 +54,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
+        new_person->parents[0] = parent0;
+        new_person->parents[0] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
     }
@@ -57,8 +64,8 @@ person *create_family(int generations)
     else
     {
         // TODO: Set parent pointers to NULL
-        new_person->parents[0] = NUll;
-        new_person->parents[1] = NUll;
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
 
