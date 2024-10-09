cfbf7174f0183bf58b4f36d5e2635eee3bf41fdf
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-09 21:50:23 +0000

    Wed, Oct 9, 2024, 11:50 PM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -84,7 +84,10 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    
+    if (p->parents[0] == NULLL && p->parents[1] == NULL)
+    {
+        free(p);
+    }
 
     // TODO: Free parents recursively
 
