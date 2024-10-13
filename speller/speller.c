b255b7079a3910a8c0fb97e16fed4645f4f56fcd
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-13 12:25:33 +0000

    Sun, Oct 13, 2024, 2:25 PM +02:00

--- speller/speller.c
+++ speller/speller.c
@@ -38,8 +38,6 @@ int main(int argc, char *argv[])
     // Load dictionary
     getrusage(RUSAGE_SELF, &before);
     bool loaded = load(dictionary);
-    getrusage(RUSAGE_SELF, &after);
-
     // Exit if dictionary not loaded
     if (!loaded)
     {
