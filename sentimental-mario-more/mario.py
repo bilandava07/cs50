9c1adb1d1afe6ea2c6e2c3b15f2395fbedf0db26
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-23 16:14:30 +0000

    Wed, Oct 23, 2024, 6:14 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,17 +1,19 @@
+# declared the function get_int myself to practise the exceptions
+
 def get_int(prompt):
-    while(True):
+    while (True):
         try:
             return int(input(prompt))
         except ValueError:
             pass
 
+
 def main():
 
-    while(True):
+    while (True):
         height = get_int("Height: ")
         if height >= 1 and height < 9:
             break
-
 
     blanks = height - 1
     hashes = 1
@@ -23,19 +25,19 @@ def main():
         for blank in range(blanks):
             print(" ", end='')
         for hash in range(hashes):
-            print("#", end ='')
+            print("#", end='')
 
-        #print the gap
+        # print the gap
         print("  ", end='')
 
         # print the right half
 
         for hash in range(hashes):
-            print("#", end ='')
+            print("#", end='')
 
         blanks -= 1
         hashes += 1
         print()
 
-main()
 
+main()
