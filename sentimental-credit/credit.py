c70ad769f15febae0685d7b61d4ea438f2f7ab7b
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-23 18:06:00 +0000

    Wed, Oct 23, 2024, 8:06 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,6 +4,8 @@ import re
 
 def main():
 
+    possibly_valid = False
+
     amex_regex = re.compile('^3[47][0-9]{13}$')
     master_regex = re.compile('^5[1-5][0-9]{14}$')
     visa_regex = re.compile('^4([0-9]{13}|[0-9]{16})$')
@@ -12,7 +14,7 @@ def main():
 
     match = amex_regex.match(card_number)
     if match:
-        break
+        possibly_valid = True
     match = master_regex.match(card_number)
     if match:
         break
