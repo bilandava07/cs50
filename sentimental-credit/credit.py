923a971c12adbb2439ec5df383aaa146901e51e0
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-23 17:55:33 +0000

    Wed, Oct 23, 2024, 7:55 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,5 +3,6 @@ import re
 
 
 def main():
-    amex_regex = re.compile('3(4|7)[0-9]{13}$')
-    master_regex = re.compile('5[1-5])
+    amex_regex = re.compile('^3[47][0-9]{13}$')
+    master_regex = re.compile('^5[1-5][0-9]{14}$')
+    visa_regex = re.compile('^4{13|16}$')
