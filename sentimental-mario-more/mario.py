7607f34578fc2e695f5323894a9346d92d944f18
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-23 16:03:51 +0000

    Wed, Oct 23, 2024, 6:03 PM +02:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,12 +1,25 @@
 def get_int(prompt):
-    try:
-        return int(input(prompt))
-    except ValueError:
-        pass
+    while(True):
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
 
 def main():
 
     height = get_int("Height: ")
 
-    for _ in range
+    blanks = height - 1
+    hashes = 1
+
+    for _ in range(height):
+        for blank in range(blanks):
+            print(" ", end='')
+        for hash in range(hashes):
+            print("#", end ='')
+        blanks -= 1
+        hashes += 1
+        print()
+
+main()
 
