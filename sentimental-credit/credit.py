98db8ea8d406bd28ad1caac7151937c3d5313696
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-23 18:00:27 +0000

    Wed, Oct 23, 2024, 8:00 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -8,7 +8,10 @@ def main():
     master_regex = re.compile('^5[1-5][0-9]{14}$')
     visa_regex = re.compile('^4([0-9]{13}|[0-9]{16})$')
 
-    
+    while(True):
+        card_number = input("Number: ")
+
+        if 
 
 
 
