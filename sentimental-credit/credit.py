f88d0adb4e531b3437c35ba0e7556b232e5594ea
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-23 19:16:09 +0000

    Wed, Oct 23, 2024, 9:16 PM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,12 +3,17 @@ import re
 def checksum(card_number):
     sum_mult2 = 0
     sum_other = 0
+    total_sum = 0
     length = len(card_number)
     # if the card number is even start with the first element to iterate through (every other number from the end )
     if length % 2 == 0:
         for i in range(0,length,2):
             sum_mult2 += card_number[i] * 2
-            
+        for y in range(1,length,2):
+            sum_other += card_number[y]
+
+        total_sum = sum_mult2 + sum_other
+
 
     else:
 
