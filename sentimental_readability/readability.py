28508688dc73c9f09702339c9be6d8785a1cd83c
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-26 18:09:49 +0000

    Sat, Oct 26, 2024, 8:09 PM +02:00

--- sentimental_readability/readability.py
+++ sentimental_readability/readability.py
@@ -3,4 +3,24 @@
 
 
 def main ():
+
+    punctuation = ['.','?','!']
+
+    # Initialize the counter variables
+
+    letters = 0
+    words = 0
+    sentences = 0
+
     text = input("Text: ")
+
+    for char in text:
+        if char.isalpha():
+            letters += 1
+        elif char == " ":
+            words += 1
+        elif char in punctuation:
+            sentences += 1
+
+    words += 1
+
