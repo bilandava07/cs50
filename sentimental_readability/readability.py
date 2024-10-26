af91b0956404b3ebe4e3a0ca14a78d60431522a6
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-26 18:18:01 +0000

    Sat, Oct 26, 2024, 8:18 PM +02:00

--- sentimental_readability/readability.py
+++ sentimental_readability/readability.py
@@ -30,7 +30,11 @@ def main ():
     index = round(0.0588 * l - 0.296 * s - 15.8)
 
     if index < 1:
-        print()
+        print("Before Grade 1")
+    elif index > 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
 
 
 
