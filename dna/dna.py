fc021d3b106883c8227f6ba37be1d0939cc97e85
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-27 17:41:45 +0000

    Sun, Oct 27, 2024, 6:41 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -43,7 +43,7 @@ def main():
             if key in str_matches.keys():
                 if value == str_matches[key]:
                     counter += 1
-        # if reached the targe amoutn of matches i.e. if all STR in str_matches are present in person's DNA exact number of times
+        # if reached the targe amoutn of matches i.e. if all STRs matched
         if counter == target_amount:
             print(entry["name"])
             return
