facf90eaf7ab73e32278872eb0ebab0e30d48764
bilandava07 <125832236+bilandava07@users.noreply.github.com>
2024-10-27 16:00:55 +0000

    Sun, Oct 27, 2024, 5:00 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -2,10 +2,11 @@ import csv
 import sys
 
 
-def main(argc, argv):
+def main():
 
     # TODO: Check for command-line usage
-
+    if len(sys.argv) != 3:
+        
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable
